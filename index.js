babd273b67b4a2fd85e45f4fab0543c90308b5f6
sadeeabdullah <sadeeabdullah016@gmail.com>
2024-09-13 07:01:07 +0600

    trying to solve a js issue on deployment

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-
-
-document.addEventListener('scroll',function(){
-
-    const parallaxSection = document.querySelectorAll('parallax')
+document.addEventListener('scroll', function() {
+    console.log("event triggered")
+    const parallaxSection = document.querySelectorAll('.parallax');
     
     parallaxSection.forEach(section => {
         const scrollPosition = window.pageYOffset;
         section.style.backgroundPositionY = (scrollPosition * 0.5) + 'px';
-    })
-})
+    });
+});
